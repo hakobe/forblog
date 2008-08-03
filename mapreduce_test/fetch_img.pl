105a6ad3d81d59808a214db59e0c3961753bce63
Yohei Fushii <hakobe@gmail.com>
2008-08-03 16:45:58 +0900

    画像ダウンロードスクリプト   - MapReduce版   - 普通の実装版

---
+++ mapreduce_test/fetch_img.pl
@@ -0,0 +1,60 @@
+package FetchImg::Mapper;
+use Moose;
+use URI;
+use Web::Scraper;
+with 'MapReduce::Lite::Mapper';
+
+my $img_scraper = scraper {
+    process 'img', 'urls[]' => '@src';
+    result 'urls';
+};
+
+sub map {
+    my ($self, $key, $value) = @_;
+    my $urls = $img_scraper->scrape( URI->new($value) );
+
+    for my $url (@$urls) {
+        $self->emit($url, $value);
+    };
+}
+
+package FetchImg::Reducer;
+use Moose;
+use URI::Fetch;
+
+with 'MapReduce::Lite::Reducer';
+
+sub reduce {
+    my ($self, $key, $values) = @_;
+
+    my $url  = URI->new($key);
+    my $name = ($url->path_segments)[-1];
+    return if $url !~ m/\.jpg$/xms;
+
+    my $res = URI::Fetch->fetch( $url );
+
+    open my $fh, '>', "datas/$name"
+        or die "File open error: $!";
+    print $fh $res->content;
+    close $fh;
+
+    $self->emit($key, $values->to_a);
+}
+
+1;
+
+package main;
+use MapReduce::Lite;
+
+my $spec = MapReduce::Lite::Spec->new(intermidate_dir => "/tmp");
+
+for (@ARGV) {
+    my $in = $spec->create_input;
+    $in->file($_);
+    $in->mapper('FetchImg::Mapper');
+}
+
+$spec->out->reducer('FetchImg::Reducer');
+$spec->out->num_tasks(3);
+
+mapreduce($spec);
---
+++ mapreduce_test/fetch_img_no_mapreduce.pl
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+use URI;
+use URI::Fetch;
+use Web::Scraper;
+
+my $img_scraper = scraper {
+    process 'img', 'urls[]' => '@src';
+    result 'urls';
+};
+
+my %all_urls = ();
+
+while ( chomp(my $site = <>) ) {
+    my $urls = $img_scraper->scrape( URI->new($site) );
+
+    for my $url (@$urls) {
+        $all_urls{$url} ||= [];
+        push @{ $all_urls{$url} }, $site;
+    }
+}
+
+for my $key (keys %all_urls) {
+    my $url = URI->new($key);
+    my $name = ($url->path_segments)[-1];
+    next if $name !~ m/\.jpg$/xms;
+
+    my $res = URI::Fetch->fetch( $url );
+
+    open my $fh, '>', "datas_no_mapreduce/$name"
+        or die "File open error: $!";
+    print $fh $res->content;
+    close $fh;
+}
